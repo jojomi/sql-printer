a6973fcca0d346c885a43c398e4b7ae7791e66c0
Johannes <dev.jojomi@yahoo.com>
2018-05-29 23:35:27 +0200

    Update Table function

--- sqlprinter.go
+++ sqlprinter.go
@@ -4,13 +4,12 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
-	"time"
 
-	"github.com/jojomi/go-spew/spew"
 	"github.com/juju/errors"
 	"github.com/olekukonko/tablewriter"
 )
 
+// Table prints the sql rows in a tabular structure
 func Table(result *sql.Rows) error {
 	var err error
 	cols, err := result.Columns()
@@ -19,22 +18,10 @@ func Table(result *sql.Rows) error {
 	}
 	headers := []string{}
 	rowContainer := make([]interface{}, 0)
-	// colTypes, _ := result.ColumnTypes()
 	for _, col := range cols {
 		headers = append(headers, col)
-		/*scanType := colTypes[i].ScanType()
-		switch scanType.Kind() {
-		case reflect.Uint32:
-			colInt := new(uint32)
-			rowContainer = append(rowContainer, colInt)
-		case reflect.Slice:
-			colInt := new([]byte)
-			rowContainer = append(rowContainer, colInt)
-		default:
-			return fmt.Errorf("unknown column type: %s for column %s", scanType.Kind(), col)
-		}*/
-		colInt := new(interface{})
-		rowContainer = append(rowContainer, colInt)
+		col := new([]byte)
+		rowContainer = append(rowContainer, col)
 	}
 
 	table := tablewriter.NewWriter(os.Stdout)
@@ -48,21 +35,14 @@ func Table(result *sql.Rows) error {
 			return errors.Annotate(err, "failed parsing result row")
 		}
 		for _, elem := range rowContainer {
-			spew.Dump(elem)
-			/*if val, ok := elem.(*uint32); ok {
-				rowData = append(rowData, fmt.Sprintf("%d", *val))
+			if elem == nil {
+				rowData = append(rowData, "<NULL>")
 				continue
-			}*/
+			}
 			if val, ok := elem.(*[]uint8); ok {
 				rowData = append(rowData, fmt.Sprintf("%s", string(*val)))
 				continue
 			}
-			if val, ok := elem.(*time.Time); ok {
-				rowData = append(rowData, fmt.Sprintf("%s", (*val).Format("2006")))
-				continue
-			}
-
-			/// rowData = append(rowData, fmt.Sprintf("%+v", elem))
 		}
 		table.Append(rowData)
 	}
